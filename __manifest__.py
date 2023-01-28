955587765ae9cc10442067bbadd6d61a6f2b6f0e
EdBernal00 <edbernal00@gmail.com>
2023-01-28 18:30:29 -0500

    base module v1.0.0

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'author': 'Eddy Bernal',
+    'sequence': -100,
+    'summary': 'Hospital management system',
+    'description': """Hospital management system.""",
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3'
+}
