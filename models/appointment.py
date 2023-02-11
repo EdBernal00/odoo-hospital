eeac9211b1d21c3b89a3d884d87c8b105d785489
EdBernal00 <edbernal@gmail.com>
2023-02-11 16:36:19 -0500

    Revert "added reference field to hospital.appointment model"
    This reverts commit e49d43bd0cb99930a08102418d283c650902de35.

--- models/appointment.py
+++ models/appointment.py
@@ -11,4 +11,3 @@ class HospitalAppointment(models.Model):
     gender = fields.Selection(related='patient_id.gender')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
-    ref = fields.Char(string='Reference')
