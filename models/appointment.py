515b95daa44f2eb661735bf64d834d416c45f466
EdBernal00 <edbernal00@gmail.com>
2023-02-14 12:50:35 -0500

    added one2many field too appointment model

--- models/appointment.py
+++ models/appointment.py
@@ -26,6 +26,7 @@ class HospitalAppointment(models.Model):
         ('done', 'Done'),
         ('cancelled', 'Cancelled')], default='draft', string='Status', required=True)
     doctor_id = fields.Many2one(comodel_name='res.users', string='Doctor', tracking=True)
+    pharmacy_line_ids = fields.One2many('appointment.pharmacy.lines', 'appointment_id', string='Pharmacy Lines')
 
     @api.onchange('patient_id')
     def onchange_patient_id(self):
@@ -56,3 +57,13 @@ class HospitalAppointment(models.Model):
     def action_draft(self):
         for rec in self:
             rec.state = 'draft'
+
+
+class AppointmentPharmacyLines(models.Model):
+    _name = "appointment.pharmacy.lines"
+    _description = "Appointment Pharmacy Lines"
+
+    product_id = fields.Many2one(comodel_name='product.product', required=True)
+    price_unit = fields.Float(related='product_id.list_price', string='Price')
+    qty = fields.Integer(string='Quantity', default=1)
+    appointment_id = fields.Many2one(comodel_name='hospital.appointment', string='Appointment')
