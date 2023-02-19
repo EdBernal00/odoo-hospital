43ebe98fa5af1d95502606af17b8c01ce6f79509
EdBernal00 <edbernal00@gmail.com>
2023-02-19 11:03:31 -0500

    created configuration menu & boolean toggle widget

---
+++ models/patient_tag.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class HospitalPatient(models.Model):
+    _name = "patient.tag"
+    _description = "Patient Tag "
+
+    name = fields.Char(string='Name', required=True)
+    active = fields.Boolean(string='Active')
+
