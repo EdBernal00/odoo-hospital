1dd423b1ccd481e74d37b8c453d13fd200215c07
EdBernal00 <edbernal00@gmail.com>
2023-02-20 14:54:26 -0500

    loaded data from xml file

--- __manifest__.py
+++ __manifest__.py
@@ -13,6 +13,7 @@
     'depends': ['mail', 'product'],
     'data': [
         'security/ir.model.access.csv',
+        'data/patient_tag_data.xml',
         'wizard/cancel_appointment_view.xml.xml',
         'views/menu.xml',
         'views/patient_view.xml',
--- models/patient_tag.py
+++ models/patient_tag.py
@@ -7,7 +7,7 @@ class HospitalPatient(models.Model):
     _description = "Patient Tag "
 
     name = fields.Char(string='Name', required=True)
-    active = fields.Boolean(string='Active')
+    active = fields.Boolean(string='Active', default=True)
     color = fields.Integer(string='Color')
     color_2 = fields.Char(string='Color 2')
 
