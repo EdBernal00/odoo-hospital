a6ea34bafc2db92d630d4458c0bc903496378ad7
EdBernal00 <edbernal00@gmail.com>
2023-02-23 22:39:59 -0500

    added sequence to hospital.patient model

--- __manifest__.py
+++ __manifest__.py
@@ -14,6 +14,7 @@
     'data': [
         'security/ir.model.access.csv',
         'data/patient_tag_data.xml',
+        'data/sequence_data.xml',
         'wizard/cancel_appointment_view.xml.xml',
         'views/menu.xml',
         'views/patient_view.xml',
