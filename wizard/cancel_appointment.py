0f98dba597ec089a00cfde74fb54b482af475505
EdBernal00 <edbernal00@gmail.com>
2023-02-26 10:50:10 -0500

    Overrited default get function on wizard

--- wizard/cancel_appointment.py
+++ wizard/cancel_appointment.py
@@ -6,6 +6,11 @@ class CancelAppointmentWizard(models.TransientModel):
     _name = "cancel.appointment.wizard"
     _description = "Cancel Appointment Wizard "
 
+    @api.model
+    def default_get(self, fields):
+        res = super(CancelAppointmentWizard, self).default_get(fields)
+        return res
+
     appointment_id = fields.Many2one('hospital.appointment', string='Appointment')
     reason = fields.Text(string='Reason')
 
